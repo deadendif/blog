5b470e16f7c1b1f23561cc53ab25127c5234c93b
deadEnding <dayupdaydown@gmail.com>
2015-12-14 22:10:30 +0800

    initial skeleton

---
+++ deadend/settings/dev.py
@@ -0,0 +1,22 @@
+# Import some utility functions
+from os.path import join
+# Fetch our common settings
+from common import *
+
+# #########################################################
+
+# ##### DEBUG CONFIGURATION ###############################
+DEBUG = True
+
+
+# ##### DATABASE CONFIGURATION ############################
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': join(PROJECT_ROOT, 'run', 'dev.sqlite3'),
+    }
+}
+
+# ##### APPLICATION CONFIGURATION #########################
+
+INSTALLED_APPS = DEFAULT_APPS
