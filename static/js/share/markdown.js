665c722d97cc7247154a4e69acf3fa3873d1ffdc
deadEnding <deadend.endif@gmail.com>
2016-06-06 00:22:07 +0800

    modify markdown js

--- static/js/share/markdown.js
+++ static/js/share/markdown.js
@@ -1,5 +1,8 @@
 /* Markdown */
 $(function(){
+    /* table style */
+    $('.markdown-body table').addClass('ui yellow striped table');
 
-    $('.markdown-body table').addClass('ui striped table');
+    /* link target */
+    $('.markdown-body a').attr('target', '_blank');
 });
